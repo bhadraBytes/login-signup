fc65237ec94b5e9890dff4c747229a89c054891d
bhadraBytes <yashTechMail@proton.me>
2023-11-03 22:35:38 +0530

    uploading

---
+++ main.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCXEq9R8YqvUS40FBddYaEfzxjldM5awlc",
+    authDomain: "authentication-app-cd869.firebaseapp.com",
+    databaseURL: "https://authentication-app-cd869-default-rtdb.firebaseio.com",
+    projectId: "authentication-app-cd869",
+    storageBucket: "authentication-app-cd869.appspot.com",
+    messagingSenderId: "730502709307",
+    appId: "1:730502709307:web:8aafebdb3bfda756d01c1e"
+  };
